7646907cecfea925778e5cf4f5e2d028c225c1ba
Bruno <brunonasccimentto@gmail.com>
2022-09-28 23:46:05 -0300

    cors atualizado

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ mongoose.connect(process.env.MONGO_URL, (err)=>{
 //     } 
 // }
 
-app.use(cors())
+app.use(cors({
+    origin: true
+  }))
 app.use(express.json())
 
 app.use("/user", userRouter)
