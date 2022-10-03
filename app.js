f927822f7f67c1b095c792dcf260bc09466ffb85
Bruno <brunonasccimentto@gmail.com>
2022-10-03 14:23:20 -0300

    cors

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const auth = require("./controllers/authController")
 const path = require("path")
 const cors = require("cors")
 
-
-
 mongoose.connect(process.env.MONGO_URL, (err)=>{
     if(err){
         console.log(err)
@@ -18,11 +16,9 @@ mongoose.connect(process.env.MONGO_URL, (err)=>{
     
 })
 
-// const options = {
-//     origin: "http://localhost:3000"
-// }
-
-app.use(cors())
+app.use(cors({
+    origin: true
+  }))
 app.use(express.json())
 
 app.use("/user", userRouter)
