24a84e7b878bb456163a1d7afca3671e820c5f1d
Bruno <brunonasccimentto@gmail.com>
2022-10-03 14:27:54 -0300

    cors

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ mongoose.connect(process.env.MONGO_URL, (err)=>{
     
 })
 
-app.use(cors({
-    origin: true
-  }))
+
+app.use(cors())
 app.use(express.json())
 
 app.use("/user", userRouter)
+
+if(process.env.DOT_DEV != "development"){
+
+app.use(express.static(path.join(__dirname, "musicApp/music/build")))
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "musicApp/music/build/index.html", function (err){
+        if(err){
+            res.status(500).redirect("user/login")
+        }
+    }))
+})
+
+}
 
 app.get("/", auth , (req, res)=>{
     
